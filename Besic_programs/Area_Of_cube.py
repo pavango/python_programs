bbdd0366f91b543ac79baca6c6e6691ac7092743
pavangowda <pavangowda9623@gmail.com>
2020-08-04 08:04:06 +0530

    modifide

--- Besic_programs/Area_Of_cube.py
+++ Besic_programs/Area_Of_cube.py
@@ -1,9 +1,5 @@
 # Program to find Area of a Cube
 
-<<<<<<< HEAD
 length = float(4)
-=======
-length = flote(4)
->>>>>>> 8af34fc8a43c728e0d0be221c0723115664b4cd3
 area = 6 * (length * length)
 print(" Surface Area of Cube = %.2f" % area)
