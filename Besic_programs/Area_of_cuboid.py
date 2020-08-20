b86aeac6005db578e79cdbed9cf7bd8986f165a1
pavangowda <pavangowda9623@gmail.com>
2020-08-20 17:27:09 +0530

    new programs

---
+++ Besic_programs/Area_of_cuboid.py
@@ -0,0 +1,6 @@
+#program tofind area of cuboid
+length = float(4)
+width = float(6)
+hight = float(4)
+area = (2 * length * hight) + (2 * width * hight ) + (2 * length * width)
+print(" Surface Area of Cube = %.2f" % area)
---
+++ Besic_programs/Area_of_cylinder.py
@@ -0,0 +1,6 @@
+#program to find area of cylineder
+hight = float(4)
+radius = float(5)
+pi = float(3.141)
+area =(2 * pi)* radius * hight + (2 * pi) *(radius * radius)
+print("Surface Area Of cylinder = %.2f" %area)
