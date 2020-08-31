3787ae614a65cdf62d3b493874c25900d9fd98a2
Pavan Gowda H.J <65288338+pavango@users.noreply.github.com>
2020-08-31 21:59:00 +0530

    Update Area_Of_Cube.py

--- besic_programs/Area_Of_Cube.py
+++ besic_programs/Area_Of_Cube.py
@@ -2,4 +2,4 @@
 
 length = int(input("Enter the value of length:"))
 area =   6 * (length * length)
-print(" Surface Area of Cube({0}) is:  {1}".format(length,area))
+print(" Surface Area of Cube({0}) is: {1}".format(length,area))
