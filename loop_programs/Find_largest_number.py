68c2e96d7395f61068ea7bf1b29fc9f5975c8c0d
pavangowda <pavangowda9623@gmail.com>
2020-09-02 20:46:50 +0530

    new program

---
+++ loop_programs/Find_largest_number.py
@@ -0,0 +1,9 @@
+a = int(input("Enter 1st number :"));
+b = int(input("Enter 2nd number :"));
+c = int(input("Enter 3rd number :"));
+if a>b and a>c:
+    print("1st value Is The Largest Number");
+if b>a and b>c:
+    print("2nd value Is The Largest Number");
+if c>a and c>b:
+    print("3rd value Is The Largest Number");
