67945bc5da534cc8cb403f5976ae2a0c9564cd28
pavangowda <pavangowda9623@gmail.com>
2020-09-07 20:53:17 +0530

    new program

--- condition_programs/Basic_Calculator.py
+++ condition_programs/Basic_Calculator.py
@@ -1,19 +1,9 @@
-
-# Python program for simple calculator
-
-# Function to add two numbers
 def add(num1, num2):
     return num1 + num2
-
-# Function to subtract two numbers
 def subtract(num1, num2):
     return num1 - num2
-
-# Function to multiply two numbers
 def multiply(num1, num2):
     return num1 * num2
-
-# Function to divide two numbers
 def divide(num1, num2):
     return num1 / num2
 
@@ -23,8 +13,6 @@ print("Please select operation -\n" \
         "3. Multiply\n" \
         "4. Divide\n")
 
-
-# Take input from the user
 select = int(input("Select operations form 1, 2, 3, 4 :"))
 
 number_1 = int(input("Enter first number: "))
