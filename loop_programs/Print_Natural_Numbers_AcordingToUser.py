9d4c02991a0db78684f3bd8cd67de0a35f5dc5c2
pavangowda <pavangowda9623@gmail.com>
2020-09-09 22:03:45 +0530

    new program

---
+++ loop_programs/Print_Natural_Numbers_AcordingToUser.py
@@ -0,0 +1,5 @@
+first_number=int(input("Enter The Starting Number :"))
+last_number=int(input("Enter The Last Number :"))
+while first_number<=last_number:
+    print(first_number)
+    first_number+=1
