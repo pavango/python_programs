e13aa4c79f77b6ffdf09f57e9403f998b9b615b1
pavangowda <pavangowda9623@gmail.com>
2020-09-09 22:25:01 +0530

    new program

---
+++ loop_programs/Add_Numbers.py
@@ -0,0 +1,7 @@
+i=0
+while(i==0):
+    num1 = int(input("Enter The 1st Number :"))
+    num2 = int(input("ENter The 2nd Number :"))
+    sum = num1+num2
+    print('The sum of {0} and {1}  is  {2}'. format(num1, num2, sum))
+    i = int(input("Enter  1 To Exit Or give 0 To Continue :"))
