e9bb3d2720440642f2dc54ebf37c248caf6a2212
pavangowda <pavangowda9623@gmail.com>
2020-09-11 22:53:20 +0530

    new program

--- condition_programs/Find_SmallestNumber.py
+++ condition_programs/Find_SmallestNumber.py
@@ -1,9 +1,9 @@
-a = int(input("Enter 1st number :"));
-b = int(input("Enter 2nd number :"));
-c = int(input("Enter 3rd number :"));
-if a>b and a>c:
+a = int(input("Enter 1st Number :"));
+b = int(input("Enter 2nd Number :"));
+c = int(input("Enter 3rd Number :"));
+if a<b and a<c:
     print("\n Ans = '1st Number Is The Smallest Number'\n");
-if b>a and b>c:
+if b<a and b<c:
     print("\n Ans = '2nd Number Is The Smallest Number'\n");
-if c>a and c>b:
+if c<a and c<b:
     print("\n Ans = '3rd Number Is The Smallest Number'\n");
--- loop_programs/Add_Numbers.py
+++ loop_programs/Add_Numbers.py
@@ -4,4 +4,4 @@ while(i==0):
     num2 = int(input("ENter The 2nd Number :"))
     sum = num1+num2
     print('The sum of {0} and {1}  is  {2}'. format(num1, num2, sum))
-    i = int(input("Enter  1 To Exit Or give 0 To Continue :"))
+    i = int(input("Enter  1 To Exit Or Give 0 To Continue :"))
