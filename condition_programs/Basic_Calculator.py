a470e058c0acf2026afa1b6c57a800820c930150
pavangowda <pavangowda9623@gmail.com>
2020-11-04 20:46:36 +0530

    new program

--- condition_programs/Basic_Calculator.py
+++ condition_programs/Basic_Calculator.py
@@ -9,10 +9,10 @@ def divide(num1, num2):
 def remainder(num1, num2):
     return num1 % num2
 print("Please Select Operation -\n" \
-        "1. Add\n" \
-        "2. Subtract\n" \
-        "3. Multiply\n" \
-        "4. Divide\n")
+    "1. Add\n" \
+    "2. Subtract\n" \
+    "3. Multiply\n" \
+    "4. Divide\n")
 
 select = int(input("Select Operations Form 1, 2, 3, 4 :"))
 
@@ -21,19 +21,19 @@ number_2 = int(input("Enter Second Number: "))
 
 if select == 1:
     print(number_1, "+", number_2, "=",
-                    add(number_1, number_2))
+    add(number_1, number_2))
 
 elif select == 2:
     print(number_1, "-", number_2, "=",
-                    subtract(number_1, number_2))
+    subtract(number_1, number_2))
 
 elif select == 3:
     print(number_1, "*", number_2, "=",
-                    multiply(number_1, number_2))
+    multiply(number_1, number_2))
 
 elif select == 4:
     print(number_1, "/", number_2, "=",
-                    divide(number_1, number_2)," Remainder Is :",
-                    remainder(number_1,number_2))
+    divide(number_1, number_2)," Remainder Is :",
+    remainder(number_1,number_2))
 else:
     print("Invalid Input")
