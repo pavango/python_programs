2208a0a6a1157ffd3e8140205bc675361dc5e543
pavangowda <pavangowda9623@gmail.com>
2020-11-10 22:07:12 +0530

    newprogram

--- besic_programs/Add_2_Numbers.py
+++ besic_programs/Add_2_Numbers.py
@@ -1,9 +1,4 @@
-# This program adds two numbers
-
 num1 = int(input("Enter the value of number_1:"))
 num2 = int(input("Enter the value of number_2:"))
-# Add two numbers
 sum = num1 + num2
-
-# Display the sum
 print('The sum of {0} and {1}  is  {2}'. format(num1, num2, sum))
